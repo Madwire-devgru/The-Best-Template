30d3a1170ef78371b3e9a9cad5075752d15c291b
Madwire-devgru <tyler.snell@madwiremedia.com>
2017-01-27 13:25:52 -0700

    Add files via upload

---
+++ Gitting/Template/js/scripts.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// animation 
+function showHide() {
+        $('#mainCat').fadeOut('slow');
+        $('#subCat').slideDown('slow');
+        $("#fluffyCat").fadeIn(3000);
+        $('#smellyCat').fadeIn(3000);
+    }
+
